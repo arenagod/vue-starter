2ff85def1674ca25cdf87141a5311b49238e600e
ado <samuelmahoon@gmail.com>
2019-12-31 04:07:45 +0800

    change footer

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer">
+  <footer class="footer margin-top-500">
     <div class="content has-text-centered">
       <strong>Starter Template</strong>
       <p>&copy; 2019</p>
@@ -12,3 +12,9 @@ export default {
 };
 
 </script>
+
+<style scoped>
+.margin-top-500 {
+  margin-top: 500px;
+}
+</style>
